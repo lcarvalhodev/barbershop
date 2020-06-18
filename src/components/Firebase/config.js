0db46b4767e1e137ef3670292825937f68666347
lcarvalhodev <lcarvalho.dev@gmail.com>
2020-06-18 00:56:25 -0300

    create login with firebase

---
+++ src/components/Firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBC9lT_ega0tD66TOYTIuMX4W8NdXnSXGY",
+    authDomain: "barbershop-89b5c.firebaseapp.com",
+    databaseURL: "https://barbershop-89b5c.firebaseio.com",
+    projectId: "barbershop-89b5c",
+    storageBucket: "barbershop-89b5c.appspot.com",
+    messagingSenderId: "1088534633238",
+    appId: "1:1088534633238:web:2cf746b82f773acae84e37",
+    measurementId: "G-H1W9RV9LJ0"
+};
+
+export default firebaseConfig;
